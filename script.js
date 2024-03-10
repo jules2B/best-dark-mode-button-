c6bb32e86b6bce441447b62cde56e43e55bb1248
Jules2B <jules2b.dev@gmail.com>
2024-03-10 22:56:31 +0100

    all

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggle_light_mode() {
+    var app = document.querySelector("body");
+    if (localStorage.lightMode == "dark") {
+        localStorage.lightMode = "light";
+        app.setAttribute("light-mode", "light");
+    } else {
+        localStorage.lightMode = "dark";
+        app.setAttribute("light-mode", "dark");
+    }
+}
+
+window.addEventListener(
+    "storage",
+    function () {
+        var app = document.querySelector("body");
+        if (localStorage.lightMode == "dark") {
+            app.setAttribute("light-mode", "dark");
+        } else {
+            app.setAttribute("light-mode", "light");
+        }
+    },
+    false
+);
